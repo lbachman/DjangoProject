b25762548c5ae0be3bb542531b6fe162b9e1b25a
Logan Bachman <lbachman@stumail.northeaststate.edu>
2024-02-06 09:23:42 -0500

    added endpoints page

--- facebook/members/views.py
+++ facebook/members/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Member
 
+
+# get request? 
 def members(request):
     # Retrieve all members from the database
     mymembers = Member.objects.all()
-
     # Pass the members to the template context
     context = {
         'members': mymembers,
     }
-
     # Render the template with the context
     return render(request, 'members/index.html', context)
